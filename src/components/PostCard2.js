294e09478c5f9740a7d566d5701314de7c082880
unknown <abdallwario96@gmail.com>
2022-02-20 13:54:18 +0300

    author

--- src/components/PostCard2.js
+++ src/components/PostCard2.js
@@ -14,7 +14,7 @@ function PostCard2(props) {
           <div className="mt-3" style={{position:'relative'}}>
             <img src="https://i.ibb.co/KFBZ2L3/profile.jpg" className="postcard-profile" />
             <div className="username-date">
-              <span className="cart-user">IsioloTimes</span>
+              <span className="cart-user">{props.post.author}   </span>
            
               <p className="cart-date">
                 {props.post.created_date}   
