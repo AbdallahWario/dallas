797d867aee09cd926ccdc68128b0e509687355b7
unknown <abdallwario96@gmail.com>
2022-02-20 17:47:57 +0300

    author

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,7 @@ function Header() {
   return (
     <header>
       <div className="container">
-        <h1>Welcome to ISIOLOTIMES</h1>
-        <p style={{ marginBottom: 0 }}>
-          ISIOLOTIMES
-          {" "}
-        </p>{" "}
-
+        
       </div>
      
     </header>
